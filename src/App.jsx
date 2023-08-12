e981b191d32e0e335d418e7a983be8d79e5f252b
David Garcia <dave74@gmail.com>
2023-08-12 14:41:52 +0200

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <h1>👋🏻 Hello World</h1>
+      <h2>venga, va, que te quiero mucho, petardo</h2>
+    </>
+  )
+}
+
+export default App
